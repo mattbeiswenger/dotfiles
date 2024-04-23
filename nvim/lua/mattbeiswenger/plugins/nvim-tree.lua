52c2e98835aab0346d28df684cfac76529f65c83
Matt Beiswenger <mattbeis@yahoo.com>
2024-04-22 19:35:23 -0500

    update mappings for nvim-tree

--- nvim/lua/mattbeiswenger/plugins/nvim-tree.lua
+++ nvim/lua/mattbeiswenger/plugins/nvim-tree.lua
@@ -19,7 +19,7 @@ return {
 
         local keymap = vim.keymap
         keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-        keymap.set("n", "<leader>ef", "<cmd>NvimTreeFocus<CR>", { desc = "Focus file explorer" })
+        keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFile<CR>", { desc = "Focus file explorer" })
         keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
         keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
 
